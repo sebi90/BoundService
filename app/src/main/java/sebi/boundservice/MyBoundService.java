487b4f07e3623c65cb8924986be91631b82cb53d
sebi90 <sebwie90@googlemail.com>
2015-06-06 17:11:57 +0200

    Initial commit

---
+++ app/src/main/java/sebi/boundservice/MyBoundService.java
@@ -0,0 +1,34 @@
+package sebi.boundservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+/**
+ * Created by Sebi on 06.06.15.
+ */
+public class MyBoundService extends Service {
+
+    private static int instanceCounter = 0;
+    private int number;
+    public MyBoundService()
+    {
+        instanceCounter++;
+        number = instanceCounter;
+    }
+    public void onCreate()
+    {
+        Log.d(this.getClass().getName(), "onCreate (" + "...");
+    }
+    public IBinder onBind(Intent intent)
+    {
+        Log.d(this.getClass().getName(), "onBind (" + "...");
+        return new CounterImpl();
+    }
+    // => gebundener Service
+    public void onDestroy()
+    {
+        Log.d(this.getClass().getName(), "onDestroy (" + "...");
+    }
+}
